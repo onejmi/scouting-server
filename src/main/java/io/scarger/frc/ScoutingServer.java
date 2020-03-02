d1de82c95929f46b99c9ce30ccc438c1da0de8e0
scarger <supremeterminal@gmail.com>
2020-03-02 17:53:30 -0500

    imported QR Code generation library

--- src/main/java/io/scarger/frc/ScoutingServer.java
+++ src/main/java/io/scarger/frc/ScoutingServer.java
@@ -1,12 +1,20 @@
 package io.scarger.frc;
 
+import java.io.IOException;
+
 import static spark.Spark.*;
 
 public class ScoutingServer {
 
     private static int counter = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String str1="192.168.0.201";
+        String str2="255.255.255.0";
+        String[] command1 = { "netsh", "interface", "ip", "set", "address",
+                "name=", "Local Area Connection" ,"source=static", "addr=",str1,
+                "mask=", str2};
+        Process pp = java.lang.Runtime.getRuntime().exec(command1);
         get("/", (req, res) -> increase());
     }
 
