4fc4aff6e3438b53d9519a6c24bad8f16e291548
scarger <supremeterminal@gmail.com>
2020-03-03 07:01:38 -0500

    removed unnecessary ip configuration code

--- src/main/java/io/scarger/frc/ScoutingServer.java
+++ src/main/java/io/scarger/frc/ScoutingServer.java
@@ -9,12 +9,6 @@ public class ScoutingServer {
     private static int counter = 0;
 
     public static void main(String[] args) throws IOException {
-        String str1="192.168.0.201";
-        String str2="255.255.255.0";
-        String[] command1 = { "netsh", "interface", "ip", "set", "address",
-                "name=", "Local Area Connection" ,"source=static", "addr=",str1,
-                "mask=", str2};
-        Process pp = java.lang.Runtime.getRuntime().exec(command1);
         get("/", (req, res) -> increase());
     }
 
